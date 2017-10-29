9a5d9b3a3a894ac7b6ff3f3cd76a5eb1e29f1da6
Raul Olmedo <raul.olmedo.dev@gmail.com>
2017-10-29 17:51:04 +0100

    Changed the MACROS defined + added a first sketch of parameters checked + test on the random distributions given in the lab paper to see how they work

--- main.cpp
+++ main.cpp
@@ -1,22 +1,46 @@
 #include <iostream>
 #include <cmath>
+#include <random>
 #include "Point.h"
 
-#define GRAVITY 6.674e-5;
-#define TIME_INTERVAL 0.1;
-#define MINIMUM_DISTANCE 2.0;
-#define SPACE_WIDTH 200;
-#define SPACE_HEIGHT 200;
-#define RAY_WIDTH 4;
-#define MASS 1000;
+#define GRAVITY 6.674e-5
+#define TIME_INTERVAL 0.1
+#define MINIMUM_DISTANCE 2.0
+#define SPACE_WIDTH 200
+#define SPACE_HEIGHT 200
+#define RAY_WIDTH 4
+#define MASS 1000
 #define SD_MASS 50
+#define PARAMETERS_REQUIRED 5
 
-// INFO: The Point class is implemented in C, maybe we could reuse it
+// INFO: The Point & distance classes are implemented natively in C++, maybe we could reuse them
 
 double distance(Point a, Point b);
 
-int main(/*int argc, char const *argv[]*/) {
+bool checkParameters(int numberOfParameters, char const *parameters[]);
+
+int main(int argc, char const *argv[]) {
     using namespace std;
+
+    // Check input
+    if (!checkParameters(argc, argv)) {
+        //INFO: commented because it is incomplete
+        //return -1;
+    };
+
+    /* Test, a ver que hacen las locuras estas de distribuciones */
+    /**************************************************************/
+    unsigned int seed = 45674;
+    // Random distributions
+    default_random_engine re{seed};
+    uniform_real_distribution<double> xdist{0.0, std::nextafter(SPACE_WIDTH, std::numeric_limits<double>::max())};
+    uniform_real_distribution<double> ydist{0.0, std::nextafter(SPACE_HEIGHT, std::numeric_limits<double>::max())};
+    normal_distribution<double> mdist{MASS, SD_MASS};
+
+    std::cout << xdist(re) << std::endl;
+    std::cout << ydist(re) << std::endl;
+    std::cout << mdist(re) << std::endl;
+    /**************************************************************/
 
     return 0;
 }
@@ -25,8 +49,27 @@ int main(/*int argc, char const *argv[]*/) {
  * This function returns the distance between the @param a and @param b
  * @param a point object
  * @param b point object
- * @return double
+ * @return double distance
  */
 double distance(Point a, Point b) {
     return sqrt(pow((a.getX() - b.getX()), 2) + pow((a.getY() - b.getY()), 2));
+}
+
+/**
+ * This function checks the parameters used in the call to the program
+ * @param numberOfParameters int number of parameters
+ * @param parameters pointer to the array with the parameters
+ * @return false if error, true if none
+ */
+bool checkParameters(int numberOfParameters, char const *parameters[]) {
+    //TODO: handle all possible inputs
+    if (numberOfParameters < PARAMETERS_REQUIRED + 1) {
+        std::cerr << "Wrong number of parameters\n" << std::endl;
+        return false;
+    }
+    for (int i = 0; i < numberOfParameters; ++i) {
+        std::cout << parameters[0] << std::endl;
+    }
+    return true;
+
 }
