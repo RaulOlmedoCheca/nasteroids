6fb10be6b23863e367366b0fdf786860461b199c
Raul Olmedo <raul.olmedo.dev@gmail.com>
2017-11-07 19:25:36 +0100

    Intento 23244 con vectores, pero visto lo visto no se como se usan jeje

--- main.cpp
+++ main.cpp
@@ -15,19 +15,15 @@
 #define SD_MASS 50
 #define PARAMETERS_REQUIRED 5
 
-// INFO: The Point & distance classes are implemented natively in C++, maybe we could reuse them
+// INFO: Lo del body y asteroid hay que revisarlo porque peta aun usando un cast
 
 bool checkParameters(int numberOfParameters, char const *parameters[]);
 
-Body** generateBodies(int numberOfAsteroids, int numberOfPlanets, unsigned int seed);
+std::vector<Body*> generateBodies(int numberOfAsteroids, int numberOfPlanets, unsigned int seed);
 
 double computeDistance(Body a, Body b);
 
 double computeAngleOfInfluence(Body a, Body b);
-
-double computeAcceleration();
-
-double computeAttractionForce(Body a, Body b);
 
 int main(int argc, char const *argv[]) {
     using namespace std;
@@ -38,22 +34,14 @@ int main(int argc, char const *argv[]) {
         //return -1;
     }
     const int num_asteroids = stoi(argv[1]);
-    const int num_iterations = stoi(argv[2]);
+    //const int num_iterations = stoi(argv[2]);
     const int num_planets = stoi(argv[3]);
     //const int pos_ray = stoi(argv[4]);
     const unsigned int seed = (unsigned int) stoi(argv[5]);
 
     // Get the array that contains the array of asteroids and planets (bodies[0] = *asteroids[], bodies[1] = *planets[])
-    Body **bodies = generateBodies(num_asteroids, num_planets, seed);
+    std::vector<Body*> bodies = generateBodies(num_asteroids, num_planets, seed);
 
-
-    for (int i = 0; i < num_iterations; ++i) {
-        for (int j = 0; j < num_asteroids; ++j) {
-            for (int k = 0; k < num_planets; ++k) {
-                computeAttractionForce(bodies[0][j], bodies[1][k]);
-            }
-        }
-    }
 
 
 
@@ -87,22 +75,23 @@ bool checkParameters(int numberOfParameters, char const *parameters[]) {
  * @param seed
  * @return
  */
-Body** generateBodies(const int numberOfAsteroids, const int numberOfPlanets, const unsigned int seed) {
+std::vector<Body*> generateBodies(const int numberOfAsteroids, const int numberOfPlanets, const unsigned int seed) {
     // Random distributions
     std::default_random_engine re{seed};
     std::uniform_real_distribution<double> xdist{0.0, std::nextafter(SPACE_WIDTH, std::numeric_limits<double>::max())};
     std::uniform_real_distribution<double> ydist{0.0, std::nextafter(SPACE_HEIGHT, std::numeric_limits<double>::max())};
     std::normal_distribution<double> mdist{MASS, SD_MASS};
 
-    Asteroid *asteroids[numberOfAsteroids];
-    Planet *planets[numberOfPlanets];
+
+    std::vector<Asteroid*> asteroids((unsigned long) numberOfAsteroids);
+    std::vector<Planet*> planets((unsigned long) numberOfAsteroids);
 
     for (int i = 0; i < numberOfAsteroids; ++i) {
-        asteroids[i] = new Asteroid(xdist(re), ydist(re), mdist(re), 0);
+        asteroids.push_back(new Asteroid(xdist(re), ydist(re), mdist(re), 0));
     }
 
     for (int j = 0; j < numberOfPlanets; ++j) {
-        planets[j] = new Planet(xdist(re), ydist(re), mdist(re) * 10);
+        planets.push_back(new Planet(xdist(re), ydist(re), mdist(re) * 10));
     }
 
     /* To access the functions inside an instance of a class
@@ -111,13 +100,10 @@ Body** generateBodies(const int numberOfAsteroids, const int numberOfPlanets, co
      * planet->getPosX();
      *
      * */
+    std::vector<std::vector<Body*> > bodies((unsigned long) numberOfAsteroids + numberOfPlanets);
+    bodies.push_back(asteroids);
 
-    // INFO: this code snippet may be wrong
-    Body **bodies[numberOfAsteroids+numberOfPlanets];
-    *bodies[0] = *asteroids;
-    *bodies[1] = *planets;
-
-    return *bodies;
+    return bodies;
 }
 
 /**
@@ -159,8 +145,14 @@ double computeAttractionForce(Body a, Body b) {
     double forceInXAxis = ((GRAVITY * a.getMass() * b.getMass()) / pow(distance, 2)) * cos(alfa);
     double forceInYAxis = ((GRAVITY * a.getMass() * b.getMass()) / pow(distance, 2)) * sin(alfa);
 
+    /*** THIS IS IMPORTANT ***/
     /* TODO: Apply the force positively for a and negatively for b, an element wont exert force to himself,
      * take care of the case in which the b Body is a planet */
 
     return forceInXAxis*forceInYAxis;
 }
+/* INFO: functions to implement: computePosition() in the main calls computeVelocity() which calls computeAcceleration()
+ * that finally calls computeAttractionForce()
+ * Hay un puto lio con las componentes para empezar, y luego cuando se calcula la fuerza de atraccion hay que aplicarla
+ * tambien al otro objeto, no tengo nada claro como implementar esto*/
+
