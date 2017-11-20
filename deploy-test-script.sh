678d5ce474021d26c828aea5f2ac32b696cd66fa
Raul Olmedo <raul.olmedo.dev@gmail.com>
2017-11-20 20:22:36 +0100

    Revert "Test error throw"
    This reverts commit e92db481219c121652647fe497eca62fd2bc2857.

--- deploy-test-script.sh
+++ deploy-test-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if ! diff init_conf.txt build/init_conf.txt || ! diff out.txt build/out.txt
+if ! diff -q init_conf.txt build/init_conf.txt || ! diff -q out.txt build/out.txt
 then
 	>&2 echo "ERROR on execution"
 fi
