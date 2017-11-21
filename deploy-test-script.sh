6fbc66c9ba7d2a34556bd134af45dfc393531766
Raul Olmedo <raul.olmedo.dev@gmail.com>
2017-11-21 11:36:55 +0100

    Added TODO and update script

--- deploy-test-script.sh
+++ deploy-test-script.sh
@@ -34,15 +34,20 @@ echo -e "\n***************** Executing nasteroids-base ***********"
 ./nasteroid-base $1 $2 $3 $4 $5
 echo -e "\n***************** Executing nasteroids-seq ************"
 ./build/nasteroids $1 $2 $3 $4 $5
-echo -e "\n\n"
-echo -e "***************** Correct output file *****************"
-cat out.txt
-echo -e "***************** Tested output file ******************"
-cat build/out.txt
-echo -e "\n***************** Correct init file *******************"
+echo -e "\n"
+echo -e "***************** Correct init file *******************"
 cat init_conf.txt
 echo -e "***************** Tested init file ********************"
 cat build/init_conf.txt
+echo -e "\n***************** Correct output file *****************"
+cat out.txt
+echo -e "***************** Tested output file ******************"
+cat build/out.txt
 echo -e "\n***************** Diff between all files **************"
 diff -q init_conf.txt build/init_conf.txt && diff -q out.txt build/out.txt
-echo "Script ended"
+echo -e "\n Removing generated files..."
+rm -rf build/out.txt
+rm -rf out.txt
+rm -rf init_conf.txt
+rm -rf build/init_conf.txt
+echo "\nScript ended"
