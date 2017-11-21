23cc48bb1cfb79bdc75d88b3b1b6c91fe4edde20
Raul Olmedo <raul.olmedo.dev@gmail.com>
2017-11-21 11:23:05 +0100

    Fixed script to use as testing

--- deploy-test-script.sh
+++ deploy-test-script.sh
@@ -1,10 +1,48 @@
 #!/bin/bash
-echo "Starting script..."
+echo -e "Starting script...\n"
+echo -e "                  +-----------------------+
+                  |          ++           |
+                  |                       |
+                  |                       |
+                  +-----------------------+
+                  +-----------------------+
+                  |                       |
+                  |     Gabo subbnormal   |
+                  |                       |
+                  |                       |
+                  |                       |
+                  |                       |
+                  |                       |
+                  |                       |
+                  |                       |
+                  |                       |
+                  |                       |
+                  |                       |
++-----------------+                       +----------------+
+|                 |                       |                |
+|                 |                       |                |
+|                 |                       |                |
+|                 |                       |                |
+|                 |                       |                |
+|                 |                       |                |
+|                 |                       |                |
+|                 |                       |                |
++-----------------+-----------------------+----------------+
+
+"
+echo -e "\n***************** Executing nasteroids-base ***********"
 ./nasteroid-base $1 $2 $3 $4 $5
+echo -e "\n***************** Executing nasteroids-seq ************"
 ./build/nasteroids $1 $2 $3 $4 $5
+echo -e "\n\n"
+echo -e "***************** Correct output file *****************"
 cat out.txt
+echo -e "***************** Tested output file ******************"
 cat build/out.txt
-cat init_file.txt
-cat build/init_file.txt
+echo -e "\n***************** Correct init file *******************"
+cat init_conf.txt
+echo -e "***************** Tested init file ********************"
+cat build/init_conf.txt
+echo -e "\n***************** Diff between all files **************"
 diff -q init_conf.txt build/init_conf.txt && diff -q out.txt build/out.txt
 echo "Script ended"
