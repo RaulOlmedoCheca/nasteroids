37118d82ca49be16fe88f6919ebb66d84c9a6bf5
Raul Olmedo <raul.olmedo.dev@gmail.com>
2017-11-21 11:03:42 +0100

    Added script

--- deploy-test-script.sh
+++ deploy-test-script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-if ! diff -q init_conf.txt build/init_conf.txt || ! diff -q out.txt build/out.txt
-then
-	>&2 echo "ERROR on execution"
-fi
+echo "Starting script..."
+./nasteroid-base $1 $2 $3 $4 $5
+./build/nasteroids $1 $2 $3 $4 $5
+cat out.txt
+cat build/out.txt
+cat init_file.txt
+cat build/init_file.txt
+diff -q init_conf.txt build/init_conf.txt && diff -q out.txt build/out.txt
+echo "Script ended"
