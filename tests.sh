92f4ff71242be1e9c688d87220f1c0d7b7171677
GuillermoEscobero <a0346060@lab.inf.uc3m.es>
2017-11-28 15:45:05 +0100

    Working version of testing script

--- tests.sh
+++ tests.sh
@@ -18,17 +18,17 @@ var9=0
 counter=1
 while [ $counter -le 10 ]
 do
-  var1=$(var1 + $(./nasteroids-seq 250 50 250 50.5 2000))
-  var2=$(var2 + $(./nasteroids-seq 250 50 500 50.5 2000))
-  var3=$(var3 + $(./nasteroids-seq 250 50 1000 50.5 2000))
+  var1=$((var1 + $(./nasteroids-seq 250 50 250 50.5 2000)))
+  var2=$((var2 + $(./nasteroids-seq 250 50 500 50.5 2000)))
+  var3=$((var3 + $(./nasteroids-seq 250 50 1000 50.5 2000)))
 
-  var4=$(var4 + $(./nasteroids-seq 500 50 250 50.5 2000))
-  var5=$(var5 + $(./nasteroids-seq 500 50 500 50.5 2000))
-  var6=$(var6 + $(./nasteroids-seq 500 50 1000 50.5 2000))
+  var4=$((var4 + $(./nasteroids-seq 500 50 250 50.5 2000)))
+  var5=$((var5 + $(./nasteroids-seq 500 50 500 50.5 2000)))
+  var6=$((var6 + $(./nasteroids-seq 500 50 1000 50.5 2000)))
 
-  var7=$(var7 + $(./nasteroids-seq 1000 50 250 50.5 2000))
-  var8=$(var8 + $(./nasteroids-seq 1000 50 500 50.5 2000))
-  var9=$(var9 + $(./nasteroids-seq 1000 50 1000 50.5 2000))
+  var7=$((var7 + $(./nasteroids-seq 1000 50 250 50.5 2000)))
+  var8=$((var8 + $(./nasteroids-seq 1000 50 500 50.5 2000)))
+  var9=$((var9 + $(./nasteroids-seq 1000 50 1000 50.5 2000)))
 
   ((counter++))
 done
@@ -58,17 +58,17 @@ var9=0
 counter=1
 while [ $counter -le 10 ]
 do
-  var1=$(var1 + $(./nasteroids-seq 250 100 250 50.5 2000))
-  var2=$(var2 + $(./nasteroids-seq 250 100 500 50.5 2000))
-  var3=$(var3 + $(./nasteroids-seq 250 100 1000 50.5 2000))
+  var1=$((var1 + $(./nasteroids-seq 250 100 250 50.5 2000)))
+  var2=$((var2 + $(./nasteroids-seq 250 100 500 50.5 2000)))
+  var3=$((var3 + $(./nasteroids-seq 250 100 1000 50.5 2000)))
 
-  var4=$(var4 + $(./nasteroids-seq 500 100 250 50.5 2000))
-  var5=$(var5 + $(./nasteroids-seq 500 100 500 50.5 2000))
-  var6=$(var6 + $(./nasteroids-seq 500 100 1000 50.5 2000))
+  var4=$((var4 + $(./nasteroids-seq 500 100 250 50.5 2000)))
+  var5=$((var5 + $(./nasteroids-seq 500 100 500 50.5 2000)))
+  var6=$((var6 + $(./nasteroids-seq 500 100 1000 50.5 2000)))
 
-  var7=$(var7 + $(./nasteroids-seq 1000 100 250 50.5 2000))
-  var8=$(var8 + $(./nasteroids-seq 1000 100 500 50.5 2000))
-  var9=$(var9 + $(./nasteroids-seq 1000 100 1000 50.5 2000))
+  var7=$((var7 + $(./nasteroids-seq 1000 100 250 50.5 2000)))
+  var8=$((var8 + $(./nasteroids-seq 1000 100 500 50.5 2000)))
+  var9=$((var9 + $(./nasteroids-seq 1000 100 1000 50.5 2000)))
   ((counter++))
 done
 counter=1
@@ -97,17 +97,17 @@ var9=0
 counter=1
 while [ $counter -le 10 ]
 do
-  var1=$(var1 + $(./nasteroids-seq 250 200 250 50.5 2000))
-  var2=$(var2 + $(./nasteroids-seq 250 200 500 50.5 2000))
-  var3=$(var3 + $(./nasteroids-seq 250 200 1000 50.5 2000))
+  var1=$((var1 + $(./nasteroids-seq 250 200 250 50.5 2000)))
+  var2=$((var2 + $(./nasteroids-seq 250 200 500 50.5 2000)))
+  var3=$((var3 + $(./nasteroids-seq 250 200 1000 50.5 2000)))
 
-  var4=$(var4 + $(./nasteroids-seq 500 200 250 50.5 2000))
-  var5=$(var5 + $(./nasteroids-seq 500 200 500 50.5 2000))
-  var6=$(var6 + $(./nasteroids-seq 500 200 1000 50.5 2000))
+  var4=$((var4 + $(./nasteroids-seq 500 200 250 50.5 2000)))
+  var5=$((var5 + $(./nasteroids-seq 500 200 500 50.5 2000)))
+  var6=$((var6 + $(./nasteroids-seq 500 200 1000 50.5 2000)))
 
-  var7=$(var7 + $(./nasteroids-seq 1000 200 250 50.5 2000))
-  var8=$(var8 + $(./nasteroids-seq 1000 200 500 50.5 2000))
-  var9=$(var9 + $(./nasteroids-seq 1000 200 1000 50.5 2000))
+  var7=$((var7 + $(./nasteroids-seq 1000 200 250 50.5 2000)))
+  var8=$((var8 + $(./nasteroids-seq 1000 200 500 50.5 2000)))
+  var9=$((var9 + $(./nasteroids-seq 1000 200 1000 50.5 2000)))
   ((counter++))
 done
 counter=1
@@ -151,17 +151,17 @@ var9=0
 counter=1
 while [ $counter -le 10 ]
 do
-  var1=$(var1 + $(./nasteroids-par 250 50 250 50.5 2000))
-  var2=$(var2 + $(./nasteroids-par 250 50 500 50.5 2000))
-  var3=$(var3 + $(./nasteroids-par 250 50 1000 50.5 2000))
+  var1=$((var1 + $(./nasteroids-par 250 50 250 50.5 2000)))
+  var2=$((var2 + $(./nasteroids-par 250 50 500 50.5 2000)))
+  var3=$((var3 + $(./nasteroids-par 250 50 1000 50.5 2000)))
 
-  var4=$(var4 + $(./nasteroids-par 500 50 250 50.5 2000))
-  var5=$(var5 + $(./nasteroids-par 500 50 500 50.5 2000))
-  var6=$(var6 + $(./nasteroids-par 500 50 1000 50.5 2000))
+  var4=$((var4 + $(./nasteroids-par 500 50 250 50.5 2000)))
+  var5=$((var5 + $(./nasteroids-par 500 50 500 50.5 2000)))
+  var6=$((var6 + $(./nasteroids-par 500 50 1000 50.5 2000)))
 
-  var7=$(var7 + $(./nasteroids-par 1000 50 250 50.5 2000))
-  var8=$(var8 + $(./nasteroids-par 1000 50 500 50.5 2000))
-  var9=$(var9 + $(./nasteroids-par 1000 50 1000 50.5 2000))
+  var7=$((var7 + $(./nasteroids-par 1000 50 250 50.5 2000)))
+  var8=$((var8 + $(./nasteroids-par 1000 50 500 50.5 2000)))
+  var9=$((var9 + $(./nasteroids-par 1000 50 1000 50.5 2000)))
 
   ((counter++))
 done
@@ -191,17 +191,17 @@ var9=0
 counter=1
 while [ $counter -le 10 ]
 do
-  var1=$(var1 + $(./nasteroids-par 250 100 250 50.5 2000))
-  var2=$(var2 + $(./nasteroids-par 250 100 500 50.5 2000))
-  var3=$(var3 + $(./nasteroids-par 250 100 1000 50.5 2000))
+  var1=$((var1 + $(./nasteroids-par 250 100 250 50.5 2000)))
+  var2=$((var2 + $(./nasteroids-par 250 100 500 50.5 2000)))
+  var3=$((var3 + $(./nasteroids-par 250 100 1000 50.5 2000)))
 
-  var4=$(var4 + $(./nasteroids-par 500 100 250 50.5 2000))
-  var5=$(var5 + $(./nasteroids-par 500 100 500 50.5 2000))
-  var6=$(var6 + $(./nasteroids-par 500 100 1000 50.5 2000))
+  var4=$((var4 + $(./nasteroids-par 500 100 250 50.5 2000)))
+  var5=$((var5 + $(./nasteroids-par 500 100 500 50.5 2000)))
+  var6=$((var6 + $(./nasteroids-par 500 100 1000 50.5 2000)))
 
-  var7=$(var7 + $(./nasteroids-par 1000 100 250 50.5 2000))
-  var8=$(var8 + $(./nasteroids-par 1000 100 500 50.5 2000))
-  var9=$(var9 + $(./nasteroids-par 1000 100 1000 50.5 2000))
+  var7=$((var7 + $(./nasteroids-par 1000 100 250 50.5 2000)))
+  var8=$((var8 + $(./nasteroids-par 1000 100 500 50.5 2000)))
+  var9=$((var9 + $(./nasteroids-par 1000 100 1000 50.5 2000)))
   ((counter++))
 done
 counter=1
@@ -230,17 +230,17 @@ var9=0
 counter=1
 while [ $counter -le 10 ]
 do
-  var1=$(var1 + $(./nasteroids-par 250 200 250 50.5 2000))
-  var2=$(var2 + $(./nasteroids-par 250 200 500 50.5 2000))
-  var3=$(var3 + $(./nasteroids-par 250 200 1000 50.5 2000))
+  var1=$((var1 + $(./nasteroids-par 250 200 250 50.5 2000)))
+  var2=$((var2 + $(./nasteroids-par 250 200 500 50.5 2000)))
+  var3=$((var3 + $(./nasteroids-par 250 200 1000 50.5 2000)))
 
-  var4=$(var4 + $(./nasteroids-par 500 200 250 50.5 2000))
-  var5=$(var5 + $(./nasteroids-par 500 200 500 50.5 2000))
-  var6=$(var6 + $(./nasteroids-par 500 200 1000 50.5 2000))
+  var4=$((var4 + $(./nasteroids-par 500 200 250 50.5 2000)))
+  var5=$((var5 + $(./nasteroids-par 500 200 500 50.5 2000)))
+  var6=$((var6 + $(./nasteroids-par 500 200 1000 50.5 2000)))
 
-  var7=$(var7 + $(./nasteroids-par 1000 200 250 50.5 2000))
-  var8=$(var8 + $(./nasteroids-par 1000 200 500 50.5 2000))
-  var9=$(var9 + $(./nasteroids-par 1000 200 1000 50.5 2000))
+  var7=$((var7 + $(./nasteroids-par 1000 200 250 50.5 2000)))
+  var8=$((var8 + $(./nasteroids-par 1000 200 500 50.5 2000)))
+  var9=$((var9 + $(./nasteroids-par 1000 200 1000 50.5 2000)))
   ((counter++))
 done
 counter=1
